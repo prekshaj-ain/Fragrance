f5bdd0b59836b82d05bae0dbae7506dc0e5a2cf8
preksha <jainpreksha2712@gmail.com>
2023-03-31 23:40:16 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Products from './Components/Products';
 function App() {
   return (
     <div className="bg-[#fffaf7]">
+      <p className='flex justify-center pt-5 font-bold text-pink-900'>BeautyBliss</p>
       <Products />
     </div>
   );
