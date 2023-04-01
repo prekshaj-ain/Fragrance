f974df2d846af464dd3f12c99cc944480b4cf41f
preksha <jainpreksha2712@gmail.com>
2023-04-01 16:00:57 +0530

    created category compoenent

---
+++ src/Components/Category.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Category = () => {
+  return (
+    <div className='my-10 py-10 flex flex-col gap-5 justify-center items-center '>
+        <h1 className='font-oliver font-bold text-3xl tracking-wider'>Scent-sational moments await</h1>
+        <div className='flex gap-1 md:w-3/5'>
+            <div className='flex-1 bg-black hover:opacity-70 z-10 opacity-100 cursor-pointer relative'>
+                <img src="/Assets/mists.jpg" alt="Mists" className='object-fill h-full w-full z-0'/>
+                <Link to='/Mist-And-Body?filter=Mist' className='absolute font-bold left-5 bottom-5 bg-white px-3 py-1 rounded-sm hover:bg-pink-300'>Mists</Link>
+            </div>
+            <div className='flex-1 bg-black hover:opacity-70 z-10 opacity-100 cursor-pointer relative'>
+                <img src="/Assets/lotions.jpg" alt="Lotions" className='object-fill h-full w-full z-5'/>
+                <Link to='/Mist-And-Body?filter=Mist' className='absolute font-bold left-5 bottom-5 bg-white px-3 py-1 rounded-sm hover:bg-pink-300'>Lotions</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Category
