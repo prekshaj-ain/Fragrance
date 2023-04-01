0938dc266def6ce47d3c8f195c020d61b0548907
preksha <jainpreksha2712@gmail.com>
2023-04-01 16:02:11 +0530

    added routing logic in app and added body component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Body from './Components/Body';
 import Products from './Components/Products';
 
 function App() {
   return (
-    <div className="bg-[#fffaf7]">
-      <p className='flex justify-center pt-5 font-bold text-pink-900'>BeautyBliss</p>
-      <Products />
+    <BrowserRouter>
+    <div className="bg-[#fcf8f6]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Body />} exact/>
+        <Route path="/:Product" element={<Products />} >
+        </Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Category from './Category'
+import Slider from './Slider'
+
+const Body = () => {
+  return (
+    <div>
+        <Slider/>
+        <Category/>
+    </div>
+  )
+}
+
+export default Body
