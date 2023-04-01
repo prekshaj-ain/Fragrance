54915f07e69168b9a456f97118bbb235cefd3e09
preksha <jainpreksha2712@gmail.com>
2023-04-01 15:55:49 +0530

    created navbar with custom font

---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='font-oliver flex justify-center font-bold text-3xl py-3 bg-white'>BeautyBliss</div>
+  )
+}
+
+export default Navbar
