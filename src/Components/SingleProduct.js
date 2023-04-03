1876df894b3360a7726d7e8a342b525c29f9538a
preksha <jainpreksha2712@gmail.com>
2023-04-03 21:58:18 +0530

    changed call to action in singleProduct

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -11,7 +11,7 @@ const SingleProduct = ({value}) => {
             <h2 className='text-2xl font-semibold'>{value.name}</h2>
             <h4 className='text-gray-700 text-[.8rem] font-bold my-1'>Fragrance Description:  <span className='text-pink-900'>{value.description}</span></h4>
             <p className='text-[.7rem] my-4'>PRICE:  {value.price}</p>
-            <a aria-label="Chat on WhatsApp" href='https://wa.me/918860398634/'> <img className='!w-28' alt="Chat on WhatsApp" src="/Assets/WhatsAppButtonGreenMedium.png" /> </a>
+            <a aria-label="Order Now" href='https://wa.me/918860398634/'> <img className='!w-28' alt="Chat on WhatsApp" src="/Assets/WhatsAppButtonGreenMedium.png" /> </a>
         </div>
     </div>
   )
