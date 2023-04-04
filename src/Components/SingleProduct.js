545f1440305633a1dce9eca7f905fb28689abe9d
preksha <jainpreksha2712@gmail.com>
2023-04-04 22:13:25 +0530

    changed styling and icon in singleProduct

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -11,7 +11,7 @@ const SingleProduct = ({value}) => {
             <h2 className='text-2xl font-semibold'>{value.name}</h2>
             <h4 className='text-gray-700 text-[.8rem] font-bold my-1'>Fragrance Description:  <span className='text-pink-900'>{value.description}</span></h4>
             <p className='text-[.7rem] my-4'>PRICE:  {value.price}</p>
-            <a aria-label="Order Now" href='https://wa.me/918860398634/'> <img className='!w-28' alt="Chat on WhatsApp" src="/Assets/WhatsAppButtonGreenMedium.png" /> </a>
+            <a className='flex h-10 items-center gap-1 bg-green-500 w-fit px-3 rounded-lg text-white text-[.8rem]' aria-label="Order Now" href='https://wa.me/918860398634/'> <img className='w-5 h-5' alt="Order Now" src="/Assets/icons8-whatsapp.svg" /><span>Order Now</span> </a>
         </div>
     </div>
   )
